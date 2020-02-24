b066c5daab36b9cd690d28acd0e650149f081f24
Stefan Davis <stefan@turnkeylinux.org>
2020-02-24 15:24:16 +1100

    Update b2evolution for 16
    - update b2evolution to 6.11.5
    - update inithooks for mysqlconf.py changes

--- overlay/usr/lib/inithooks/bin/b2evolution.py
+++ overlay/usr/lib/inithooks/bin/b2evolution.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Set b2evolution admin password and email
 
 Option:
@@ -16,7 +16,7 @@ from random import SystemRandom
 from string import ascii_letters, digits
 
 from dialog_wrapper import Dialog
-from mysqlconf import MySQL, escape_chars
+from mysqlconf import MySQL
 
 random = SystemRandom()
 
@@ -25,16 +25,16 @@ def randomkey():
 
 def usage(s=None):
     if s:
-        print >> sys.stderr, "Error:", s
-    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
-    print >> sys.stderr, __doc__
+        print("Error:", s, file=sys.stderr)
+    print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
+    print(__doc__, file=sys.stderr)
     sys.exit(1)
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
                                        ['help', 'pass=', 'email='])
-    except getopt.GetoptError, e:
+    except getopt.GetoptError as e:
         usage(e)
 
     password = ""
@@ -65,12 +65,12 @@ def main():
     inithooks_cache.write('APP_EMAIL', email)
 
     salt = ''.join(random.choice(ascii_letters+digits) for i in range(8)) 
-    hash = hashlib.md5(salt+password).hexdigest()
+    hash = hashlib.md5((salt+password).encode('utf8')).hexdigest()
 
     m = MySQL()
     username = 'admin'
-    m.execute('UPDATE b2evolution.users SET user_pass=\"%s\", user_email=\"%s\", user_unsubscribe_key=\"%s\", user_salt=\"%s\" WHERE user_login=\"%s\";' % \
-	(escape_chars(hash), email, randomkey(), salt, username))
+    m.execute('UPDATE b2evolution.users SET user_pass=%s, user_email=%s, user_unsubscribe_key=%s, user_salt=%s WHERE user_login=%s;', \
+	(hash, email, randomkey(), salt, username))
 
 
 if __name__ == "__main__":
